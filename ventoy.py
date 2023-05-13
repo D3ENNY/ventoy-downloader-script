7935267cb0e5ef56c1342a42e07b5c4fee699dad
D3ENNY <denysraimondi06@gmail.com>
2023-05-13 10:53:39 +0200

    idk man

--- ventoy.py
+++ ventoy.py
@@ -3,21 +3,16 @@
 from requests import get
 from subprocess import run
 from glob import glob
-from os import chdir
+from os import chdir, makedirs
+from os.path import exists
 from re import search
-
-def getFile():
-    path = 'download/ventoy/'
-    chdir(path)
-    file = glob("*.zip")
-    file = path+file[0]
-    chdir('../../')
-    return file
+from shutil import rmtree
 
 
-def downloadFile(url):
-    run(['rm','-rf', 'download/ventoy'])
-    run(['wget','-P','download/ventoy', url])
+
+def downloadFile(url, path):
+    rmtree(path)
+    run(['wget','-P', path, url])
 
 def getVersion():
     res = get('https://github.com/ventoy/Ventoy/releases')
@@ -29,15 +24,18 @@ def getVersion():
     
 print('---ventoy.py---')
 version = getVersion()
+path='download/ventoy/'
+if not exists(path):
+    makedirs(path)
 # Watch out: garbage code ahead!
 try:
     with open('version/ventoy-version.txt', 'r+') as file:
         if file.read() != version:
             file.truncate(0)
             file.write(version)
-            downloadFile(f'https://github.com/ventoy/Ventoy/releases/download/v{version}/ventoy-{version}-windows.zip')
+            downloadFile(f'https://github.com/ventoy/Ventoy/releases/download/v{version}/ventoy-{version}-windows.zip', path)
 
 except FileNotFoundError:
     with open('version/ventoy-version.txt', 'w') as file:
         file.write(version)
-        downloadFile(f'https://github.com/ventoy/Ventoy/releases/download/v{version}/ventoy-{version}-windows.zip')
+        downloadFile(f'https://github.com/ventoy/Ventoy/releases/download/v{version}/ventoy-{version}-windows.zip', path)
